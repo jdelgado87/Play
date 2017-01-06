3572d882da177a307214b13ca37574d45150b1f6
Jose Delgado <pianodude10@yahoo.com>
2017-01-05 22:44:34 -0800

    Count change

--- Play/ViewController.swift
+++ Play/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
         
         tapCount = tapCount + 1
         
-        if tapCount >= 10 {
+        if tapCount >= 20 {
            theLabel.text = "You tapped the button 10 times"
         }
         
